2acdb6efb2f763e846c244e1d19d973961e59453
F3rnando14 <fernando.verificade@outlook.com>
2026-05-27 15:38:00 -0600

    Ya nmms tiene que  funcionar

---
+++ TEST/Anticithera_Backend_Source/src/main/java/com/anticithera/backend/config/OptionsResource.java
@@ -0,0 +1,15 @@
+
+package com.anticithera.backend.config;
+
+import jakarta.ws.rs.OPTIONS;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("{path : .*}")
+public class OptionsResource {
+
+    @OPTIONS
+    public Response options() {
+        return Response.ok().build();
+    }
+}
